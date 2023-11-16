a358f372a6f7552354c23863deeac1c9781dac23
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-11-16 22:58:02 +0530

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,55 @@
+import logo from '../Assets/logo.png';
+import cart_icon from '../Assets/cart_icon.png';
+
 import './Navbar.css';
+import { useState } from 'react';
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const [menu, SetMenu] = useState('shop');
+
+  return (
+    <div className="navbar">
+      <div className="nav_logo">
+        <img src={logo} alt="" />
+        <p>SHOPPER</p>
+      </div>
+      <ul className="nav_menu">
+        <li
+          onClick={() => {
+            SetMenu('Shop');
+          }}
+        >
+          Shop {menu === 'Shop' ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            SetMenu('Mens');
+          }}
+        >
+          Mens {menu === 'Mens' ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            SetMenu('Womens');
+          }}
+        >
+          Womens {menu === 'Womens' ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            SetMenu('Kids');
+          }}
+        >
+          Kids {menu === 'Kids' ? <hr /> : <></>}
+        </li>
+      </ul>
+      <div className="nav_login_cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav_cart_count">0</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
