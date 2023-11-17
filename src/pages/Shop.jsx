98f4897d05b7c84d546ffd7c29ac2e7eec31b185
Faiznhmd <fiaznhmd.0624@gmail.com>
2023-11-17 14:04:36 +0530

    hero done

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,7 +1,12 @@
+import Hero from '../components/hero/Hero';
 import './Shop.css';
 
 const Shop = () => {
-  return <div>Shop</div>;
+  return (
+    <div>
+      <Hero />
+    </div>
+  );
 };
 
 export default Shop;
